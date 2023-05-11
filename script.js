9d6f4ebc703d17054aad8f30e8fcca11772fca24
haseebasif <haseebasif215@gmail.com>
2023-05-11 13:28:52 +0500

    App Completed

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 const hamburger = document.querySelector(".hamburger");
 const navLinks = document.querySelector(".nav-links");
 
-hamburger.addEventListener("click",function(){
-    hamburger.classList.toggle("active")
-    navLinks.classList.toggle("responsive");
-})
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navLinks.classList.toggle("responsive");
+});
+
+// Smooth scrolling
+const navItems = document.querySelectorAll(".nav-links a");
+
+navItems.forEach(function (navItem) {
+  navItem.addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const target = document.querySelector(navItem.getAttribute("href"));
+    const offsetTop = target.offsetTop;
+
+    window.scrollTo({
+      top: offsetTop,
+      behavior: "smooth",
+    });
+  });
+});
